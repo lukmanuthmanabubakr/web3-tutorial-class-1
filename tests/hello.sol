95d655451d85435317fd3dbe5765016cc19069cc
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-28 00:50:41 +0100

    progress

--- tests/hello.sol
+++ tests/hello.sol
@@ -7,11 +7,16 @@ contract MyFirstSmartContract{
     // uint256 public no = 81;
 
 
-     string public hey;
-    uint256 public no;
+   string public hey;
+   uint256 public no;
 
-    constructor(string memory _hey, uint _no){
-        hey = _hey;
-        no = _no;
-    }
+//    constructor(string memory _hey, uint _no) {
+//         hey = _hey;
+//         no = _no;
+//    }
+
+  function updateInfo (string memory _hey, uint _no) public  {
+    hey = _hey;
+    no = _no;
+  }
 }
