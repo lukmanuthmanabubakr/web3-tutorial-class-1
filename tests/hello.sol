c975e50df87092b8cc87827edd0d14948db0d72a
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-28 00:41:37 +0100

    alhadullilah

--- tests/hello.sol
+++ tests/hello.sol
@@ -3,6 +3,15 @@
 pragma solidity ^0.8.25;
 
 contract MyFirstSmartContract{
-    string public abu = "Abu is a good boy";
-    uint256 public abuBro = 81;
+    // string public hey = "Abu is a good boy";
+    // uint256 public no = 81;
+
+
+     string public hey;
+    uint256 public no;
+
+    constructor(string memory _hey, uint _no){
+        hey = _hey;
+        no = _no;
+    }
 }
