0f89691c9a2f1d088987078b7cc52c9d77450cd2
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-28 13:20:46 +0100

    lets-move

---
+++ tests/firstApp.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+contract NFTCount {
+    uint public numberOfNFT;
+
+    //This function will icrement the NFT Number
+
+    function addNFT() public {
+        
+    }
+}
