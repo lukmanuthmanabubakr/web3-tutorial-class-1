c710dfa107568ec8f3b5302f9cd3d1999986aaf4
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-29 08:33:53 +0100

    lets practice

---
+++ tests/Practice.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.25;
+
+contract PracticeesterdayClass {
+    uint256 public NFTCounter;
+
+    //to make increment
+
+    function IncreaseNFT () public  {
+        NFTCounter += 1;
+    }
+
+    //To decrement NFT
+
+    function decrement
+}
