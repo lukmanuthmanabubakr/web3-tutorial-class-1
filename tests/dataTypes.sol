663a23c10f53c7a92455a6d5084e3d3d9ffa5d88
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-04-29 09:03:53 +0100

    sol error

--- tests/dataTypes.sol
+++ tests/dataTypes.sol
@@ -1,3 +1,16 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.25;
+
+contract DataTypes {
+    bool public hey;
+    bool public no = true;
+
+    //UINT stands for unsigned integer, meaning non negative integers differen size are available
+
+    uint8 // range from 0 -2 ** 8 - 1  // 255
+    uint16 // range from 0 -2 ** 16 - 1 // 65535
+
+
+    uint256 //ranges 0 - 2 ** 256 - 1 // 1.1579209e+77
+}
