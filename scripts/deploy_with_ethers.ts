b07fd6ba38694ef5844ac0c5abeadae2866db5a8
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-04-30 08:03:28 +0100

    Update deploy_with_ethers.ts

--- scripts/deploy_with_ethers.ts
+++ scripts/deploy_with_ethers.ts
@@ -1,4 +1,4 @@
-// This script can be used to deploy the "Storage" contract using ethers.js library.
+// This script can be used to de
 // Please make sure to compile "./contracts/1_Storage.sol" file before running this script.
 // And use Right click -> "Run" from context menu of the file to run the script. Shortcut: Ctrl+Shift+S
 
