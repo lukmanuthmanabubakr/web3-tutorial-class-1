886bc5b44e75760f82b61c04b007d16ba681a922
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-01 10:36:11 +0100

    uint is used for only positive number

--- tests/dataTypes.sol
+++ tests/dataTypes.sol
@@ -8,9 +8,15 @@ contract DataTypes {
 
     //UINT stands for unsigned integer, meaning non negative integers differen size are available
 
-    uint8 // range from 0 -2 ** 8 - 1  // 255
-    uint16 // range from 0 -2 ** 16 - 1 // 65535
+    // uint8 // range from 0 -2 ** 8 - 1  // 255
+    // uint16  // range from 0 -2 ** 16 - 1 // 65535
 
 
-    uint256 //ranges 0 - 2 ** 256 - 1 // 1.1579209e+77
+    // uint256 //ranges 0 - 2 ** 256 - 1 // 1.1579209e+77
+
+    uint8 public u8 = 1;
+    uint public u256 = 456;
+    uint public u = 123; //uint is a alias from uint256
+
+
 }
