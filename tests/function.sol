e820423908473c125151fe95456d349ea5f77288
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-03 10:57:18 +0100

    let make an update

--- tests/function.sol
+++ tests/function.sol
@@ -13,4 +13,9 @@ contract LearnFunction {
     function updateData(uint256 _hey) public{
         hey = _hey;
     }
+
+    function get(uint256 _a, uint256 _b) public view returns(uint){
+        uint256 newNumber = _a + _b;
+        return newNumber;
+    }
 }
