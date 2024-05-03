2780637957745fd25840513e5fab8d56ebb6be70
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-03 10:53:49 +0100

    let make an update

--- tests/function.sol
+++ tests/function.sol
@@ -2,10 +2,15 @@
 pragma solidity 0.8.25;
 
 contract LearnFunction {
-    uint256 public  hey;
+    // uint256 public  hey;
+    uint256 hey;
 
     //get data with the help of function
     function getInfo() public view returns(uint256) {
         return hey;
+    }
+
+    function updateData(uint256 _hey) public{
+        hey = _hey;
     }
 }
