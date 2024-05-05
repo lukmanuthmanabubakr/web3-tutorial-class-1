fef832c8b48ef928bc06d21a4588d3a8e203ee15
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-05 10:07:56 +0100

    let begin

--- tests/hello.sol
+++ tests/hello.sol
@@ -3,20 +3,20 @@
 pragma solidity ^0.8.25;
 
 contract MyFirstSmartContract{
-    // string public hey = "Abu is a good boy";
-    // uint256 public no = 81;
+    string public hey = "Abu is a good boy";
+    uint256 public no = 81;
 
 
-   string public hey;
-   uint256 public no;
+  //  string public hey;
+  //  uint256 public no;
 
 //    constructor(string memory _hey, uint _no) {
 //         hey = _hey;
 //         no = _no;
 //    }
 
-  function updateInfo (string memory _hey, uint _no) public  {
-    hey = _hey;
-    no = _no;
-  }
+  // function updateInfo (string memory _hey, uint _no) public  {
+  //   hey = _hey;
+  //   no = _no;
+  // }
 }
