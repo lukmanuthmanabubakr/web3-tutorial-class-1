93889deff15308bfc78c29e5890821ce88916328
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-05 10:43:11 +0100

    constructor

--- tests/hello.sol
+++ tests/hello.sol
@@ -3,17 +3,17 @@
 pragma solidity ^0.8.25;
 
 contract MyFirstSmartContract{
-    string public hey = "Abu is a good boy";
-    uint256 public no = 81;
+    // string public hey = "Abu is a good boy";
+    // uint256 public no = 81;
 
 
-  //  string public hey;
-  //  uint256 public no;
+   string public hey;
+   uint256 public no;
 
-//    constructor(string memory _hey, uint _no) {
-//         hey = _hey;
-//         no = _no;
-//    }
+   constructor(string memory _hey, uint _no) {
+        hey = _hey;
+        no = _no;
+   }
 
   // function updateInfo (string memory _hey, uint _no) public  {
   //   hey = _hey;
--- tests/practice.sol
+++ tests/practice.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.8.25;
 
 contract Mypractice {
-    string public mindset = "I want to be come a web3 genius";
-    uint256 public myWorth = 10000000;
+    // string public mindset = "I want to be come a web3 genius";
+    // uint256 public myWorth = 10000000;
+
+    string public school;
+    uint256 public level;
+
+    constructor (string memory _school, uint _level) {
+        school = _school;
+        level = _level;
+    }
+
 }
