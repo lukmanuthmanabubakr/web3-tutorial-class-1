03e3ea65b88efae8b2936651101dd55f69f9ccfe
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-05 10:17:38 +0100

    ok

---
+++ tests/practice.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract Mypractice {
+    string public mindset = "I want to be come a web3 genius";
+    uint256 public myWorth = 10000000;
+}
