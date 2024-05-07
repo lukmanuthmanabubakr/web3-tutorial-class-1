eeb6bd515a8e8a01f5a2e197fb65cc2527839dad
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-07 18:30:44 +0100

    lets practice the function

--- tests/function.sol
+++ tests/function.sol
@@ -10,9 +10,9 @@ contract LearnFunction {
         return hey;
     }
 
-    function updateData(uint256 _hey) public{
-        hey = _hey;
-    }
+    // function updateData(uint256 _hey) public{
+    //     hey = _hey;
+    // }
 
     // function get(uint256 _a, uint256 _b) public view returns(uint){
     //     uint256 newNumber = _a + _b;
