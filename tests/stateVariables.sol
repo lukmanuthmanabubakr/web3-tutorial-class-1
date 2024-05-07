48c28118b8d9e586a867fce7c43f60c70f21d514
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-07 19:34:58 +0100

    lets dive into state Variable

---
+++ tests/stateVariables.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract StateVariable {
+    //I HAVE TO KEEP FEW THINGS IN MIND ABOUT STATE VARIABLES
+
+
+    string public myState;
+    uint256 public myNum;
+
+    string public defaultText = "Hey Default text";
+    uint256 public defaultNum = 5;
+
+}
