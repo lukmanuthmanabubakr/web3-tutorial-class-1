5354b99af004c33757c139d22204d4ad65b134b7
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-10 11:42:24 +0100

    update

--- tests/localVariables.sol
+++ tests/localVariables.sol
@@ -2,5 +2,28 @@
 pragma solidity ^0.8.25;
 
 contract LocalVariables {
-    
+
+    uint256 public myNumber;
+
+    //FUCTION FOR LOCAL VARIABLES
+    // function local () public pure returns(uint256) {
+    //     //Variable define inside the fuction scope
+    //     //Not stored on the blockchain
+
+
+    //     uint256 i = 345;
+    //     return i;
+    // }
+
+
+        function local () public returns(uint256) {
+        //Variable define inside the fuction scope
+        //Not stored on the blockchain
+
+
+        uint256 i = 345;
+        myNumber = i;
+        return myNumber;
+    }
+
 }
