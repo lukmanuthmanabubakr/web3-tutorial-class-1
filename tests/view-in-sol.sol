be55152341e3401389a2df4edc18619f4b0d972c
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-17 18:22:06 +0100

    somehow getting it

--- tests/view-in-sol.sol
+++ tests/view-in-sol.sol
@@ -2,6 +2,20 @@
 pragma solidity ^0.8.25;
 
 contract View {
-    uint256 num1 = 2;
-    uint256 num2 = 7;
+    uint256 Num1 = 2;
+    uint256 Num2 = 7;
+
+    // USING VIEW TO CHECK THE STATE VARIABLE
+    // function getResults() public view returns (uint256, uint256){
+    //     return (Num1,Num2);
+    // }
+
+    //What if i want to calculate
+    function getResults() public view returns (uint256 product, uint256 total) {
+        uint256 num1 = 20;
+        uint256 num2 = 30;
+
+        product = num1 * num2;
+        total = num1 + num2;
+    }
 }
