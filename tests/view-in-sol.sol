95a5781b8ab7aaffb40e3af442f2f90482eca09a
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-05-18 20:12:08 +0100

    error solved

--- tests/view-in-sol.sol
+++ tests/view-in-sol.sol
@@ -11,14 +11,16 @@ contract View {
     // }
 
     //What if i want to calculate
-    function getResults() public view returns (uint256 product, uint256 total) {
+    function getResults() public view returns (uint256, uint256) {
         // uint256 num1 = 20;
         // uint256 num2 = 30;
 
-        Num1 += 5;
-        Num2 += 7;
+        // Num1 += 5;
+        // Num2 += 7;
 
-        product = Num1 * Num2;
-        total = Num1 + Num2;
+       uint256 product = Num1 * Num2;
+       uint256 total = Num1 + Num2;
+
+        return (product, total);
     }
 }
